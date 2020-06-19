ccbba17323f692e79707127e0ef3a8abc2781215
Ray Hunter <ray.hunter@gmail.com>
2020-06-19 08:11:48 -0700

    add display pages

---
+++ pages/reservations/_confirmationCode/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    this is reservation {{ reservation.city }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    reservation: {
+      type: Object,
+      default: () => {
+        return { city: 'Boston' }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
