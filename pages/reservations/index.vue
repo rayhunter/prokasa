81cb473671333be14daf30ecd2b0accecea987e0
Ray Hunter <ray.hunter@gmail.com>
2020-06-22 17:34:26 -0700

    add home btn to rsrvtns index

--- pages/reservations/index.vue
+++ pages/reservations/index.vue
@@ -2,16 +2,27 @@
   <div class="container is-fluid">
     <section class="section">
       <label class="label">Reservations</label>
-      <div class="control is-large">
-        <div class="select">
-          <select class="select is-info" @change="getReservation($event)">
-            <option value="" selected disabled>
-              Select reservation
-            </option>
-            <option v-for="reservation in reservations" :key="reservation.confirmationCode" :value="reservation.confirmationCode">
-              {{ reservation.city }}
-            </optIon>
-          </select>
+      <div class="columns">
+        <div class="column 1-half">
+          <div class="control is-large">
+            <div class="select">
+              <select class="select is-info" @change="getReservation($event)">
+                <option value="" selected disabled>
+                  Select reservation
+                </option>
+                <option v-for="reservation in reservations" :key="reservation.confirmationCode" :value="reservation.confirmationCode">
+                  {{ reservation.city }}
+                </optIon>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div class="column 1-half">
+          <nuxt-link :to="`../`">
+            <button class="button">
+              Home
+            </button>
+          </nuxt-link>
         </div>
       </div>
     </section>
